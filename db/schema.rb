73dc0814034575d70ff74261212689a99121b0fc
Michael Abbott <michael@mjabbottdesign.com>
2021-08-04 08:44:39 -0700

    Create Competitions and CompetitionTeams migrations; update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_162719) do
+ActiveRecord::Schema.define(version: 2021_08_04_152601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "amusement_parks", force: :cascade do |t|
+    t.string "name"
+    t.float "admission_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "competition_teams", force: :cascade do |t|
+    t.bigint "competition_id"
+    t.bigint "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["competition_id"], name: "index_competition_teams_on_competition_id"
+    t.index ["team_id"], name: "index_competition_teams_on_team_id"
+  end
+
+  create_table "competitions", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "sport"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mechanics", force: :cascade do |t|
+    t.string "name"
+    t.integer "years_of_experience"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "players", force: :cascade do |t|
     t.string "name"
@@ -24,6 +55,25 @@ ActiveRecord::Schema.define(version: 2021_08_03_162719) do
     t.index ["team_id"], name: "index_players_on_team_id"
   end
 
+  create_table "ride_mechanics", force: :cascade do |t|
+    t.bigint "ride_id"
+    t.bigint "mechanic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mechanic_id"], name: "index_ride_mechanics_on_mechanic_id"
+    t.index ["ride_id"], name: "index_ride_mechanics_on_ride_id"
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.string "name"
+    t.integer "thrill_rating"
+    t.boolean "open"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "amusement_park_id"
+    t.index ["amusement_park_id"], name: "index_rides_on_amusement_park_id"
+  end
+
   create_table "teams", force: :cascade do |t|
     t.string "hometown"
     t.string "nickname"
@@ -31,5 +81,10 @@ ActiveRecord::Schema.define(version: 2021_08_03_162719) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "competition_teams", "competitions"
+  add_foreign_key "competition_teams", "teams"
   add_foreign_key "players", "teams"
+  add_foreign_key "ride_mechanics", "mechanics"
+  add_foreign_key "ride_mechanics", "rides"
+  add_foreign_key "rides", "amusement_parks"
 end
